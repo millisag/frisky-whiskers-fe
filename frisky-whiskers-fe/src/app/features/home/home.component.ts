86ec5b899176701a0cb9dad1fac5de6140777b0e
Millisa G <millisa419@yahoo.com>
2025-09-04 13:34:44 -0400

    Debugging in class

--- frisky-whiskers-fe/src/app/features/home/home.component.ts
+++ frisky-whiskers-fe/src/app/features/home/home.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CatService } from '../../core/services/cat.service';
+import { CatListComponent } from '../../shared/cats/cat-list/cat-list.component';
+import { Cat } from '../../shared/models/cat';
 
 @Component({
+  standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [CatListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  cats = [];
+  cats: Cat[] = [];
 
   constructor(private catService: CatService) {}
 
   ngOnInit(): void {
     this.catService.getCats().subscribe({
       next: (cats) => {
+        this.cats = cats;
         console.log(cats);
       },
       error: (error) => {
--- frisky-whiskers-fe/src/app/shared/cats/cat-list/cat-list.component.ts
+++ frisky-whiskers-fe/src/app/shared/cats/cat-list/cat-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-cat-list',
   imports: [],
   templateUrl: './cat-list.component.html',
   styleUrl: './cat-list.component.scss'
 })
 export class CatListComponent {
-
+  @Input() cats: any[] = [];
 }
